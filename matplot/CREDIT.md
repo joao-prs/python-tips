9d48dc1bdaea11cf067c5eecbedb6542daaae8d9
joao-prs <kandrocmatunein@gmail.com>
2025-06-23 20:55:14 -0300

    add: matplot

---
+++ matplot/CREDIT.md
@@ -0,0 +1,2 @@
+créditos aos dados públicos
+- https://dados.gov.br/dados/conjuntos-dados
---
+++ matplot/main.py
@@ -0,0 +1,31 @@
+# $ yay -S python-matplotlib
+# $ yay -S python-pandas
+# $ yay -S python-seaborn
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+dados = pd.read_csv('agua_2022.csv', sep=';')
+
+dados['Quantidade (m3)'] = pd.to_numeric(dados['Quantidade (m3)'], errors='coerce')
+
+# tabela de campus
+plt.figure(figsize=(10, 6))
+sns.scatterplot(
+    data=dados,
+    y='Quantidade (m3)',
+    x='Mes',
+    hue='Campus/Unidade',   # random cor
+    palette='tab10',        # cores
+    s=100                   # ponto
+)
+
+plt.xlabel('Quantidade (m³)')
+plt.ylabel('Mes')
+plt.title('Dispersão por Campus/Unidade')
+plt.grid(True)
+plt.legend(title='Campus')
+plt.tight_layout()
+plt.show()
